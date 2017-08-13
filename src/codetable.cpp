ecada8cce6f1b3e0afdd9dba2546534e27382dae
pixxxel <ivan.n.sergeev@gmail.com>
2017-08-13 20:52:04 +0500

    Implement CodeTable class

--- src/codetable.cpp
+++ src/codetable.cpp
@@ -17,8 +17,27 @@ CodeTable::CodeTable(byte low, byte high, byte* iniTable) {
 		if (code == hBound) break;
 	}
 	if (iniTable) {
-		//
+		for (byte idx = 0; idx <= high - lBound && iniTable[idx]; idx++) {
+			array[idx] = iniTable[idx];
+		}
 	}
 }
 
+CodeTable::CodeTable(const CodeTable &src) {
+	array = new byte[(hBound=src.hBound) - (lBound=src.lBound) + 1];
+	for (byte idx = lBound; ; idx++) {
+		array[idx - lBound] = src.array[idx - lBound];
+		if (idx == hBound) break;
+	}
+}
 
+CodeTable::~CodeTable() {
+	delete[] array;
+}
+
+byte& CodeTable::operator[] (byte i) {
+	if (i < lBound || i > hBound) {
+		throw std::out_of_range("Index is out of range");
+	}
+	return array[i - lBound];
+}
